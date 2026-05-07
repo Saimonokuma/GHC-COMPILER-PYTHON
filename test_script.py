2ada7028d71e5773833545a1af926113b6de1e52
Saimonokuma <saimonflor@gmail.com>
2026-05-07 17:49:26 +0200

    Delete test_script.py
    Temporary...

--- test_script.py
+++
@@ -1,9 +0,0 @@
-import re
-
-content = ""
-with open("ghc_compiler_python/wrapper.py", "r") as f:
-    content = f.read()
-
-# Let's see if we missed any missing check=True
-if "subprocess.run(" in content:
-    print("Found subprocess.run")
